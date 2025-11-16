e8c19c3ab63b92b4e1f188c6630d137f8b933fe7
ExploreTutorialsOfficial <khushigandhi16211@gmail.com>
2025-11-16 18:14:27 +0530

    Update project.js

--- project.js
+++ project.js
@@ -22,7 +22,7 @@ getPlanBtn.addEventListener("click", async () => {
 
 const getRecommendations = async (message) => {
 
-  const API_KEY = "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"; 
+  const API_KEY = "xx"; 
   const API_URL = "https://api.groq.com/openai/v1/chat/completions";
 
   const requestOptions = {
@@ -55,5 +55,6 @@ body: JSON.stringify({
 function refreshPage() {
   location.reload(); // Reload the current page
 }
+
 
 
